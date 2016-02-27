24bc6026b8bd1093d53aceb08573f65c349f6b87
Leo Suarez <Leo@DTRaleigh.com>
2016-02-27 11:01:41 -0500

    Register snapshot_admin with the Admin

--- eats/admin.py
+++ eats/admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
-from eats.models import District, Business
+from eats.models import District, Business, snapshot
 
-# admin.site.register(District)
-class DistrictAdmin(admin.ModelAdmin):
+class district_admin(admin.ModelAdmin):
     fields = ['name', 'description', 'link', 'photo', 'district_map']
     list_display = ('name', 'link')
 
-# admin.site.register(Business)
-class BusinessAdmin(admin.ModelAdmin):
+class business_admin(admin.ModelAdmin):
     fields = ['name', 'district', 'link', 'description',
                'display_on_site', 'has_outdoor_seating', 'is_temp_closed',
               'is_eats', 'is_drinks', 'is_coffee', 'not_local',
@@ -25,5 +23,11 @@ class BusinessAdmin(admin.ModelAdmin):
     make_not_local.short_description = "Mark selected as not local"
     make_local.short_description = "Mark selected as local"
 
-admin.site.register(District, DistrictAdmin)
-admin.site.register(Business, BusinessAdmin)
+class snapshot_admin(admin.ModelAdmin):
+    fields = ['date', 'local_business_count', 'not_local_business_count',
+                'business_list']
+    list_display = ['date', 'local_business_count', 'not_local_business_count']
+
+admin.site.register(District, district_admin)
+admin.site.register(Business, business_admin)
+admin.site.register(snapshot, snapshot_admin)
