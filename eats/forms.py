11c9d35d7881748fe3abfa11c5b63ff8c0e31192
Leo Suarez <Leo@DTRaleigh.com>
2016-04-17 08:03:12 -0400

    Add JQuery calendar picker to all date fields.

--- eats/forms.py
+++ eats/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.forms import ModelForm
 from eats.models import Business, tip, reference_link
 
@@ -5,11 +6,15 @@ class edit_business_form(ModelForm):
     class Meta:
         model = Business
         exclude = ['description' , 'latitude', 'longitude']
+        widgets = {'open_date': forms.DateInput(attrs={'type':'date'}),
+                    'close_date': forms.DateInput(attrs={'type':'date'})}
 
 class new_business_form(ModelForm):
     class Meta:
         model = Business
         exclude = ['description' , 'latitude', 'longitude']
+        widgets = {'open_date': forms.DateInput(attrs={'type':'date'}),
+                    'close_date': forms.DateInput(attrs={'type':'date'})}
 
 class new_tip_form(ModelForm):
     class Meta:
@@ -20,8 +25,10 @@ class edit_tip_form(ModelForm):
     class Meta:
         model = tip
         fields = '__all__'
+        widgets = {'open_date': forms.DateInput(attrs={'type':'date'}) }
 
 class create_ref_link_form(ModelForm):
     class Meta:
         model = reference_link
         fields = '__all__'
+        widgets = {'date_published': forms.DateInput(attrs={'type':'date'}) }
