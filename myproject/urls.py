ac252d4b799f514de650c0a680af2e40b9e84288
Leo Suarez <leo@dtraleigh.com>
2020-05-25 10:45:33 -0400

    Updated from url() to path()

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,21 +1,22 @@
-from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path
+
 from eats import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', views.home),
-    url(r'^debug/$', views.debug),
-    url(r'^tips/$', views.tips_main),
-    url(r'^manage/$', views.eats_login),
-    url(r'^manage/main/$', views.main),
-    url(r'^manage/main/logout/$', views.eats_logout),
-    url(r'^manage/add_business/$', views.add_business),
-    url(r'^manage/edit/biz/(?P<biz_id>[0-9]+)$', views.edit_business),
-    url(r'^manage/add_vendor/$', views.add_vendor),
-    url(r'^manage/edit/vendor/(?P<vendor_id>[0-9]+)$', views.edit_vendor),
-    url(r'^manage/tips/$', views.tips_page),
-    url(r'^manage/tips/(?P<tip_id>[0-9]+)$', views.edit_tips_page),
-    url(r'^manage/tips/reference_link/$', views.ref_link_page),
-    url(r'^manage/tips/reference_link/(?P<ref_id>[0-9]+)$', views.edit_ref_link_page),
+    path("admin/", admin.site.urls),
+    path("", views.home),
+    path("debug/", views.debug),
+    path("tips/", views.tips_main),
+    path("manage/", views.eats_login),
+    path("manage/main/", views.main),
+    path("manage/main/logout/", views.eats_logout),
+    path("manage/add_business/", views.add_business),
+    path("manage/edit/biz/<int:biz_id>", views.edit_business),
+    path("manage/add_vendor/$", views.add_vendor),
+    path("manage/edit/vendor/<int:vendor_id>", views.edit_vendor),
+    path("manage/tips/", views.tips_page),
+    path("manage/tips/<int:tip_id>", views.edit_tips_page),
+    path("manage/tips/reference_link/", views.ref_link_page),
+    path("manage/tips/reference_link/<int:ref_id>", views.edit_ref_link_page),
 ]
